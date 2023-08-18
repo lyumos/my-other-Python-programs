47db1c8346989fed203fee27527c894a73793eaa
lyumos <63208434+lyumos@users.noreply.github.com>
2023-08-18 14:04:58 +0300

    test_task updated

--- test_task.py
+++ test_task.py
@@ -1,13 +1,13 @@
 def calculate_min_score(x: int, y: int, z: int) -> int:
     score_diff = abs(x-y) #разница (модуль) между очками
     z_max_score_diff = z - max(x, y) #сколько при хорошем раскладе нужно набрать для победы (для max(x, y))
-    if score_diff < 2: #если плотная борьба
+    if score_diff >= 2 and z <= max(x, y):
+        return 0 #один из участников уже выиграл
+    elif score_diff < 2: #если плотная борьба
         # 2 - score_diff -> сколько нужно набрать (потерять) очков для разницы в 2
-        min_score_to_win = max(z_max_score_diff, 2 - score_diff)
-    else:
-        min_score_to_win = z_max_score_diff
-
-    return min_score_to_win
+        return max(z_max_score_diff, 2 - score_diff)
+    else: #отрыв в очках есть, но z еще не достигнут
+        return z_max_score_diff
 
 
 if __name__ == '__main__':
